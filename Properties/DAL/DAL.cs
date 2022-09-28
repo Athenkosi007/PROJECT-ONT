9f7e76ca25fa542d6f8717896d4ca7428c3d9fa8
Athenkosi007 <114588448+Athenkosi007@users.noreply.github.com>
2022-09-28 15:09:05 +0200

    Add files via upload

---
+++ Properties/DAL/DAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Cities //4
+    {
+        public int CityID { get; set; }
+        public string CityDescription { get; set; }
+        public int ProvinceID { get; set; }
+
+    }
+    public class Surbub //5
+    {
+        public int SurbubID {get; set; }
+        public string SurbubDescription { get; set; }
+        public int PostalCode { get; set; }
+        public int CityID { get; set; }
+
+    }
+    public class Agencies //6
+    {
+        public int AgencyID { get; set; }
+        public string AgencyName { get; set; }
+        public int SurbubID { get; set; }
+
+    }
+
+
+}
---
+++ Properties/DAL/DataAccessLayer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DataAccessLayer
+    {
+        static string ConnString = "Data Source = localhost; Initial Catalog = RentalDB;  Integrated Security = true;";
+        SqlConnection dbConn = new SqlConnection(ConnString);
+        SqlCommand dbComm;
+        SqlDataAdapter dbAdapter;
+        DataTable dt;
+
+        public int AddCity(Cities city)
+        {
+            dbConn.Open();
+            dbComm = new SqlCommand("AddCity_sp", dbConn);
+            dbComm.CommandType = CommandType.StoredProcedure;
+
+            dbComm.Parameters.AddWithValue("@CityDescription", city.CityDescription);
+            dbComm.Parameters.AddWithValue("@Province", city.ProvinceID);
+
+            if (dbConn.State == ConnectionState.Closed)
+            {
+                dbConn.Open();
+            }
+
+            int x = dbComm.ExecuteNonQuery();
+            return x;
+        }
+    }
+
+}
