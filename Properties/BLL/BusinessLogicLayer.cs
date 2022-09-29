a3a64dfc244a06f5f3b1c42b0e439ffb25d7fa78
Majila <s224378449@mandela.ac.za>
2022-09-29 22:42:54 +0200

    bll

--- Properties/BLL/BusinessLogicLayer.cs
+++ Properties/BLL/BusinessLogicLayer.cs
@@ -8,5 +8,74 @@ namespace BLL
 {
     public class BusinessLogicLayer
     {
+        public int AddProvince()
+        {
+            return dal.AddProvince();
+        }
+        public DataTable ListProvince() 
+        {
+            return dal.ListProvince();
+        }
+        public DataTable UpdateProvince()
+        {
+            return dal.UpdateProvince();
+        }
+        public int AddCity(City city)
+        {
+            return dal.AddCity(city);
+        }
+        public DataTable ListCity()
+        {
+            return dal.ListCity();
+        }
+        public DataTable UpdateCity(City city)
+        {
+            return dal.UpdateCity(city);
+        }
+        public  int AddSurbub(Surbub sub)
+        {
+            return dal.AddSurbub(sub);
+        }
+        public DataTable ListSurbub()
+        {
+            return dal.ListSurbub();
+        }
+        public DataTable DeleteSurbub(Surbub sub)
+        {
+            return dal.DeleteSurbub(sub);
+        }
+        public DataTable cmbSurbub()
+        {
+            return dal.cmbSurbub();
+        }
+        public DataTable cmbProvince()
+        {
+            return dal.cmbProvince();
+        }
+        public DataTable cityReport(string desc)
+        {
+            return dal.cityReport(desc);
+        }
+        public DataTable cmbCity()
+        {
+            return dal.cmbCity();
+        }
+
+ public int AddProperty(Property prop)
+        {
+            return dal.AddProperty(prop);
+        }
+        public DataTable ListProperty()
+        {
+            return dal.ListProperty();
+        }
+        public DataTable UpdatePropety(Property prop)
+        {
+            return dal.UpdateProperty(clas);
+        }
+        public DataTable DeleteProperty(Property prop)
+        {
+           return dal.DeleteProperty(clas);
+        }
     }
 }
