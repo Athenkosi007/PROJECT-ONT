db0dbdbc0a902ee6d2a587641b39440e37a713b7
Mbasa <s224378449@mandela.ac.za>
2022-10-05 11:05:26 +0200

    proptype push again

--- Properties/Properties/Property.Designer.cs
+++ Properties/Properties/Property.Designer.cs
@@ -260,14 +260,14 @@
             // 
             this.panel1.Location = new System.Drawing.Point(2, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(582, 407);
+            this.panel1.Size = new System.Drawing.Size(914, 407);
             this.panel1.TabIndex = 28;
             // 
             // Property
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 411);
+            this.ClientSize = new System.Drawing.Size(916, 417);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnUpdate);
